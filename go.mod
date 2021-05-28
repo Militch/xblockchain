936e856e471ef536fba5ca58e83241b6bc6997d4
Lanyuan Bert <lanyuanbert@LanyuandeMacBook-Pro.local>
2021-05-28 19:11:56 +0800

    bert http and websocket

--- go.mod
+++ go.mod
@@ -6,14 +6,16 @@ require (
 	github.com/btcsuite/btcutil v1.0.2
 	github.com/dgraph-io/badger/v3 v3.2011.1
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-resty/resty/v2 v2.5.0
+	github.com/go-resty/resty/v2 v2.6.0
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/google/flatbuffers v2.0.0+incompatible // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jackpal/bencode-go v1.0.0 // indirect
 	github.com/nictuku/dht v0.0.0-20201226073453-fd1c1dd3d66a // indirect
 	github.com/perlin-network/noise v1.1.3
 	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.3
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	github.com/youtube/vitess v2.1.1+incompatible // indirect
 	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 )
--- rpc/server_starter.go
+++ rpc/server_starter.go
@@ -5,51 +5,51 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"go/token"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"net/rpc"
 	"reflect"
 	"strings"
 	errors2 "xblockchain/rpc/errors"
+
+	"github.com/gorilla/websocket"
 )
 
 var typeOfError = reflect.TypeOf((*error)(nil)).Elem()
 
 type ServerStarter struct {
-	rpcserver *rpc.Server
+	rpcserver  *rpc.Server
 	serviceMap map[string]*service
 }
 
 type methodType struct {
-	method     reflect.Method
-	ArgType    reflect.Type
-	ReplyType  reflect.Type
-	numCalls   uint
+	method    reflect.Method
+	ArgType   reflect.Type
+	ReplyType reflect.Type
+	numCalls  uint
 }
 
 type service struct {
-	name   string                  // name of service
-	rcvr   reflect.Value           // receiver of methods for the service
-	typ    reflect.Type            // type of the receiver
+	name    string                 // name of service
+	rcvr    reflect.Value          // receiver of methods for the service
+	typ     reflect.Type           // type of the receiver
 	methods map[string]*methodType // registered methods
 }
-
-
-
 
 func (s *service) Call(methodName string, params interface{}) (interface{}, error) {
 	mtype := s.methods[methodName]
 	if mtype == nil {
-		return nil, errors2.New(-32601,"Method not found")
+		return nil, errors2.New(-32601, "Method not found")
 	}
 	function := mtype.method.Func
 	argIsValue := false
 	var argv reflect.Value
 	if mtype.ArgType.Kind() == reflect.Ptr {
 		argv = reflect.New(mtype.ArgType.Elem())
-	}else {
+	} else {
 		argv = reflect.New(mtype.ArgType)
 		argIsValue = true
 	}
@@ -62,15 +62,15 @@ func (s *service) Call(methodName string, params interface{}) (interface{}, erro
 		case reflect.Slice:
 			paramsArr, _ := params.([]interface{})
 			if len(paramsArr) != argv.NumField() {
-				return nil, errors2.New(-32602,"Invalid params")
+				return nil, errors2.New(-32602, "Invalid params")
 			}
-			for i:=0; i<argv.NumField(); i++ {
+			for i := 0; i < argv.NumField(); i++ {
 				argv.Field(i).Set(reflect.ValueOf(paramsArr[i]))
 			}
 		case reflect.Map:
 			paramsMap := params.(map[string]interface{})
 			if len(paramsMap) != argv.NumField() {
-				return nil, errors2.New(-32602,"Invalid params")
+				return nil, errors2.New(-32602, "Invalid params")
 			}
 			for k, v := range paramsMap {
 				argvv := argv.FieldByName(k)
@@ -97,7 +97,6 @@ func (s *service) Call(methodName string, params interface{}) (interface{}, erro
 	return replyv.Interface(), nil
 }
 
-
 func NewServerStarter() (*ServerStarter, error) {
 	return &ServerStarter{
 		serviceMap: make(map[string]*service),
@@ -118,29 +117,43 @@ func suitableMethods(typ reflect.Type) map[string]*methodType {
 		method := typ.Method(m)
 		mtype := method.Type
 		mname := method.Name
-		if method.PkgPath != "" { continue }
-		if mtype.NumIn() != 3 { continue }
+		if method.PkgPath != "" {
+			continue
+		}
+		if mtype.NumIn() != 3 {
+			continue
+		}
 		argType := mtype.In(1)
-		if !isExportedOrBuiltinType(argType) { continue }
+		if !isExportedOrBuiltinType(argType) {
+			continue
+		}
 		replyType := mtype.In(2)
-		if replyType.Kind() != reflect.Ptr { continue }
-		if !isExportedOrBuiltinType(replyType) { continue }
-		if mtype.NumOut() != 1 { continue }
-		if returnType := mtype.Out(0); returnType != typeOfError { continue }
+		if replyType.Kind() != reflect.Ptr {
+			continue
+		}
+		if !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+		if mtype.NumOut() != 1 {
+			continue
+		}
+		if returnType := mtype.Out(0); returnType != typeOfError {
+			continue
+		}
 		methods[mname] = &methodType{
-			method: method,
-			ArgType: argType,
+			method:    method,
+			ArgType:   argType,
 			ReplyType: replyType,
 		}
 	}
 	return methods
 
 }
-func (server *ServerStarter) Register(rcvr interface{}) error  {
+func (server *ServerStarter) Register(rcvr interface{}) error {
 	return server.register(rcvr, "", false)
 }
 
-func (server *ServerStarter) RegisterName(name string,rcvr interface{}) error  {
+func (server *ServerStarter) RegisterName(name string, rcvr interface{}) error {
 	return server.register(rcvr, name, true)
 }
 
@@ -169,19 +182,19 @@ func (server *ServerStarter) register(rcvr interface{}, name string, useName boo
 	return nil
 }
 
-func perParseRequestData(c *gin.Context) (map[string]interface{},error) {
-	if "POST" != c.Request.Method {
+func perParseRequestData(c *http.Request) (map[string]interface{}, error) {
+	if "POST" != c.Method {
 		return nil, errors.New("POST method excepted")
 	}
-	if nil == c.Request.Body {
+	if nil == c.Body {
 		return nil, fmt.Errorf("no POST data")
 	}
-	body, err := ioutil.ReadAll(c.Request.Body)
+	body, err := ioutil.ReadAll(c.Body)
 	if err != nil {
 		return nil, fmt.Errorf("errors while reading request body")
 	}
-	data := make(map[string]interface{})
-	decoder := json.NewDecoder(bytes.NewReader(body))
+	var data = make(map[string]interface{})
+	decoder := json.NewDecoder(bytes.NewBuffer(body))
 	decoder.UseNumber()
 	err = decoder.Decode(&data)
 	if nil != err {
@@ -190,33 +203,32 @@ func perParseRequestData(c *gin.Context) (map[string]interface{},error) {
 	return data, nil
 }
 
-
-func (server *ServerStarter) handleJsonRPCRequest(c *gin.Context) (*int, interface{}, error)  {
-	data, err := perParseRequestData(c)
-	if err != nil {
-		return nil, nil, errors2.New(-32700,"Parse error")
+func (server *ServerStarter) handleJsonRPCRequest(data map[string]interface{}) (*int, interface{}, error) {
+	// data, err := perParseRequestData(c)
+	if len(data) == 0 || data != nil {
+		return nil, nil, errors2.New(-32700, "Parse error")
 	}
 	idNumber, ok := data["id"].(json.Number)
 	if !ok {
-		return nil, nil, errors2.New(-32600,"Invalid Request")
+		return nil, nil, errors2.New(-32600, "Invalid Request")
 	}
 	id64, err := idNumber.Int64()
 	if err != nil {
-		return nil, nil, errors2.New(-32600,"Invalid Request")
+		return nil, nil, errors2.New(-32600, "Invalid Request")
 	}
 	id := int(id64)
 	if data["jsonrpc"] != "2.0" {
-		return &id, nil,  errors2.New(-32600,"Invalid Request")
+		return &id, nil, errors2.New(-32600, "Invalid Request")
 	}
 	method, ok := data["method"].(string)
-	mpake := strings.Split(method,".")
-	if !ok || len(mpake) != 2  {
-		return &id,nil, errors2.New(-32601,"Method not found")
+	mpake := strings.Split(method, ".")
+	if !ok || len(mpake) != 2 {
+		return &id, nil, errors2.New(-32601, "Method not found")
 	}
 	params := data["params"]
 	service := server.serviceMap[mpake[0]]
 	if service == nil {
-		return &id,nil, errors2.New(-32601,"Method not found")
+		return &id, nil, errors2.New(-32601, "Method not found")
 	}
 	result, err := service.Call(mpake[1], params)
 	if err != nil {
@@ -225,26 +237,77 @@ func (server *ServerStarter) handleJsonRPCRequest(c *gin.Context) (*int, interfa
 	return &id, result, nil
 }
 
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func (server *ServerStarter) Run(addr string) error {
-	//gin.SetMode(gin.ReleaseMode)
-	//gin.DefaultWriter = ioutil.Discard
-	r := gin.Default()
-	r.POST("/", func(c *gin.Context) {
-		id, data,err := server.handleJsonRPCRequest(c)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != "POST" {
+			ws, err := upGrader.Upgrade(w, r, nil)
+			if err != nil {
+				return
+			}
+			defer ws.Close()
+			for {
+				//读取ws中的数据
+				mt, message, err := ws.ReadMessage()
+				if err != nil {
+					break
+				}
+				jsonMap := JSONToMap(string(message))
+				id, data, err := server.handleJsonRPCRequest(jsonMap)
+
+				infoMap := make(map[string]interface{})
+				if err != nil {
+					infoMap["jsonrpc"] = "2.0"
+					infoMap["error"] = err
+					infoMap["id"] = id
+				} else {
+					infoMap["jsonrpc"] = "2.0"
+					infoMap["id"] = id
+					infoMap["result"] = data
+				}
+				mjson, _ := json.Marshal(infoMap)
+				//写入ws数据
+				err = ws.WriteMessage(mt, mjson)
+				if err != nil {
+					break
+				}
+			}
+		}
+		asd, _ := perParseRequestData(r)
+		id, data, err := server.handleJsonRPCRequest(asd)
+		var infoDataJson = make(map[string]interface{})
 		if err != nil {
-			c.JSON(200, gin.H{
-				"jsonrpc": "2.0",
-				"error": err,
-				"id": id,
-			})
+			infoDataJson["jsonrpc"] = "2.0"
+			infoDataJson["error"] = err
+			infoDataJson["id"] = id
+			mjson, _ := json.Marshal(infoDataJson)
+			io.WriteString(w, string(mjson))
 			return
 		}
-		c.JSON(200, gin.H{
-			"jsonrpc": "2.0",
-			"id": id,
-			"result": data,
-		})
+		infoDataJson["jsonrpc"] = "2.0"
+		infoDataJson["id"] = id
+		infoDataJson["result"] = data
+		mjson, _ := json.Marshal(infoDataJson)
+		io.WriteString(w, string(mjson))
 	})
-	return r.Run(addr)
+	return http.ListenAndServe(addr, nil)
+}
+
+func JSONToMap(str string) map[string]interface{} {
+
+	var tempMap map[string]interface{}
+
+	err := json.Unmarshal([]byte(str), &tempMap)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return tempMap
 }
