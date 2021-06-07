4fddf4c632b5911c2d27cdce026d79977818c744
Lanyuan Bert <lanyuanbert@LanyuandeMacBook-Pro.local>
2021-06-07 19:18:08 +0800

    https http ws

--- cmd/xblockchain/sub/daemon.go
+++ cmd/xblockchain/sub/daemon.go
@@ -2,34 +2,37 @@ package sub
 
 import (
 	"fmt"
-	"github.com/nictuku/dht"
-	"github.com/spf13/cobra"
 	"log"
 	"os"
 	"xblockchain"
 	"xblockchain/api"
+	configs "xblockchain/cmd/config"
 	"xblockchain/rpc"
 	"xblockchain/storage/badger"
+
+	"github.com/nictuku/dht"
+	"github.com/spf13/cobra"
 )
 
 var (
 	daemonCmd = &cobra.Command{
-		Use:   "daemon",
+		Use: "daemon",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runDaemon()
 		},
 	}
 )
 
-
 func runDaemon() error {
-	keyStorage := badger.New("./data0/keys")
+
+	tempConfig := configs.GetConfig() // 配置获取
+	keyStorage := badger.New(tempConfig.Blockchain.Keys)
 	defer func() {
 		if err := keyStorage.Close(); err != nil {
 			log.Fatalf("Key Storage close errors: %s", err)
 		}
 	}()
-	blocksStorage := badger.New("./data0/blocks")
+	blocksStorage := badger.New(tempConfig.Blockchain.Blocks)
 	defer func() {
 		if err := blocksStorage.Close(); err != nil {
 			log.Fatalf("Blocks Storage close errors: %s", err)
@@ -40,13 +43,13 @@ func runDaemon() error {
 
 	ws := xblockchain.NewWallets(keyStorage)
 	gopt := xblockchain.DefaultGenesisBlockOpts()
-	bc,err := xblockchain.NewBlockChain(gopt, blocksStorage)
+	bc, err := xblockchain.NewBlockChain(gopt, blocksStorage)
 	if err != nil {
 		log.Fatalf("blockchain initail err: %s", err)
 	}
 
 	miner := xblockchain.NewMiner(bc, ws, txPendingPool)
-	starter,err := rpc.NewServerStarter()
+	starter, err := rpc.NewServerStarter()
 	if err != nil {
 		log.Fatalf("RPC server starter initail err: %s", err)
 		return err
@@ -63,9 +66,9 @@ func runDaemon() error {
 		Wallets: ws,
 	}
 
-	txApiHandler := &api.TXAPIHandler {
-		Wallets: ws,
-		BlockChain: bc,
+	txApiHandler := &api.TXAPIHandler{
+		Wallets:       ws,
+		BlockChain:    bc,
 		TxPendingPool: txPendingPool,
 	}
 	err = starter.RegisterName("Chain", chainApiHandler)
@@ -91,12 +94,11 @@ func runDaemon() error {
 		log.Fatalf("RPC service register error: %s", err)
 		return err
 	}
-	err = starter.Run(":9005")
+	err = starter.Run()
 	if err != nil {
 		log.Fatalf("RPC server run error: %s", err)
 		return err
 	}
-
 
 	//server := net.Default()
 	//server.StartAndListen("0.0.0.0:198")
@@ -118,14 +120,13 @@ func RunP2PPeer() {
 	go drainresults(d)
 }
 
-
 func drainresults(n *dht.DHT) {
 	count := 0
 	for r := range n.PeersRequestResults {
 		for _, peers := range r {
 			for _, x := range peers {
 				//fmt.Printf("%d: %v\n", count, dht.DecodePeerAddress(x))
-				log.Printf("join node: %s\n",dht.DecodePeerAddress(x))
+				log.Printf("join node: %s\n", dht.DecodePeerAddress(x))
 				count++
 				//if count >= 10 {
 				//	log.Printf("RPC server running by listen :9005\n")
